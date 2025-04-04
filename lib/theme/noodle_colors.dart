f7d650a9e252c5a370432e64bbc5806037fbd416
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-04 21:08:57 +0900

    feat: 폰트 및 이미지 에셋 추가

---
+++ lib/theme/noodle_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NoodleColors {
+  // Primary
+  static const Color primary = Color(0xFFFF7E12);
+  static const Color primaryLight = Color(0xFFFFF2E7);
+
+  // Secondary
+  static const Color secondaryBlue = Color(0xFFB3D4F1);
+  static const Color secondaryGray = Color(0xFF757575);
+
+  // 상태 강조
+  static const Color error = Color(0xFFE53935);
+  static const Color disabled = Color(0xFFBDBDBD);
+
+  // Background
+  static const Color backgroundWhite = Color(0xFFFFFFFF);
+  static const Color backgroundLight = Color(0xFFF2F3F3);
+  static const Color backgroundOverlay = Color(0xD9000000);
+  static const Color selectedBackground = Color(0xFFFFE0B2);
+
+  // Text
+  static const Color textDefault = Color(0xFF000000);
+  static const Color textPrimary = Color(0xDD000000);
+  static const Color textSecondary = Color(0x8A000000);
+}
