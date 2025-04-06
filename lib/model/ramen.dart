bdcd95ac8c22e1182f8269ee99176532a25d9f87
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-06 17:36:24 +0900

    feat: 라면 모델(Ramen) 생성

---
+++ lib/model/ramen.dart
@@ -0,0 +1,31 @@
+class Ramen {
+  final int id;
+  final String name;
+  final String imageUrl;
+  final String spicyLevel;
+  final String description;
+  final String recipe;
+  final bool afterSeasoning;
+
+  Ramen({
+    required this.id,
+    required this.name,
+    required this.imageUrl,
+    required this.spicyLevel,
+    required this.description,
+    required this.recipe,
+    required this.afterSeasoning,
+  });
+
+  factory Ramen.fromJson(Map<String, dynamic> json) {
+    return Ramen(
+      id: json['ramenIndex'],
+      name: json['ramenName'],
+      imageUrl: json['ramenImage'],
+      spicyLevel: json['ramenSpicy'],
+      description: json['ramenDescription'],
+      recipe: json['ramenRecipe'],
+      afterSeasoning: json['afterSeasoning'],
+    );
+  }
+}
