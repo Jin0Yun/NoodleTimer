543007434b6275bfc0195e5d827675c48975f1eb
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-06 21:39:16 +0900

    refactor: RamenCategoryFilter 컴포넌트에 RamenBrand 모델 데이터 연동
    - 기존 String 배열방식에서 RamenBrand 모델 기반으로 변경
    - 카테고리 텍스트 → ramenBrands[index].name 방식으로 개선

--- lib/screen/home/ramen/ramen_category_filter.dart
+++ lib/screen/home/ramen/ramen_category_filter.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:noodle_timer/model/ramen_brand.dart';
 import 'package:noodle_timer/theme/noodle_colors.dart';
 import 'package:noodle_timer/theme/noodle_text_styles.dart';
 
 class RamenCategoryFilter extends StatelessWidget {
   final int selectedIndex;
   final ValueChanged<int> onTap;
-  final List<String> categories;
+  final List<RamenBrand> ramenBrands;
 
   const RamenCategoryFilter({
     required this.selectedIndex,
     required this.onTap,
-    required this.categories,
+    required this.ramenBrands,
     super.key,
   });
 
@@ -21,7 +22,7 @@ class RamenCategoryFilter extends StatelessWidget {
       child: ListView.separated(
         scrollDirection: Axis.horizontal,
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        itemCount: categories.length,
+        itemCount: ramenBrands.length,
         separatorBuilder: (_, __) => const SizedBox(width: 8),
         itemBuilder: (context, index) {
           final isSelected = index == selectedIndex;
@@ -33,18 +34,18 @@ class RamenCategoryFilter extends StatelessWidget {
               padding: const EdgeInsets.symmetric(horizontal: 12),
               decoration: BoxDecoration(
                 color:
-                    isSelected
-                        ? NoodleColors.primaryLight
-                        : NoodleColors.secondaryGray,
+                isSelected
+                    ? NoodleColors.primaryLight
+                    : NoodleColors.secondaryGray,
                 borderRadius: BorderRadius.circular(8),
               ),
               child: Text(
-                categories[index],
+                ramenBrands[index].name,
                 style: NoodleTextStyles.titleXSmBold.copyWith(
                   color:
-                      isSelected
-                          ? NoodleColors.primary
-                          : NoodleColors.categoryTabText,
+                  isSelected
+                      ? NoodleColors.primary
+                      : NoodleColors.categoryTabText,
                 ),
               ),
             ),
