d9ccadfc4988c638d7cb9af358824ce8e46e4c93
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-06 21:43:13 +0900

    feat: ProviderScope 추가로 Riverpod 상태 관리 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:noodle_timer/screen/home/home_screen.dart';
 import 'package:noodle_timer/screen/onboarding/noodle_preference_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,8 +10,10 @@ void main() async {
   final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
 
   runApp(
-    MaterialApp(
-      home: isFirstLaunch ? const NoodlePreferenceScreen() : const HomeScreen(),
+    ProviderScope(
+      child: MaterialApp(
+        home: isFirstLaunch ? const NoodlePreferenceScreen() : const HomeScreen(),
+      ),
     ),
   );
 }
