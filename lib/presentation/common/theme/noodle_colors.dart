2316dcfc5bbd5403a4ba51e559770a2d5258ef0c
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-10 19:38:20 +0900

    refactor: 컬러 네이밍 변경

--- lib/presentation/common/theme/noodle_colors.dart
+++ lib/presentation/common/theme/noodle_colors.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 
 class NoodleColors {
-  // Primary
+  // 메인 컬러 (Primary)
   static const Color primary = Color(0xFFFF7E12);
   static const Color primaryLight = Color(0xFFFFF2E7);
 
-  // Secondary
-  static const Color secondaryGray = Color(0xFFE8E8E8);
-  static const Color secondaryDarkGray = Color(0xFF9E9E9E);
+  // 중립 컬러 (Neutral)
+  static const Color neutral100 = Color(0xFFFFFFFF); // 가장 밝은 화이트
+  static const Color neutral200 = Color(0xFFF5F5F5); // 연한 배경색
+  static const Color neutral300 = Color(0xFFF2F2F2); // 검색창 배경
+  static const Color neutral400 = Color(0xFFE8E8E8); // 연한 그레이
+  static const Color neutral500 = Color(0xFFE0E0E0); // 경계선 그레이
+  static const Color neutral600 = Color(0xFFD6D6D6); // 버튼 배경 그레이
+  static const Color neutral700 = Color(0xFFBDBDBD); // 비활성화 그레이
+  static const Color neutral800 = Color(0xFF9E9E9E); // 어두운 그레이
+  static const Color neutral900 = Color(0xFF8D8D8D); // 탭 텍스트용 그레이
+  static const Color neutral1000 = Color(0xFF212121); // 가장 어두운 텍스트용
 
-  // 상태 강조
+  // 상태 표시 컬러
   static const Color error = Color(0xFFE53935);
-  static const Color disabled = Color(0xFFBDBDBD);
-
-  // Background
-  static const Color backgroundWhite = Color(0xFFFFFFFF);
-  static const Color backgroundLight = Color(0xFFF5F5F5);
-  static const Color backgroundOverlay = Color(0xD9000000);
-  static const Color selectedBackground = Color(0xFFFFE0B2);
-
-  // Text
-  static const Color categoryTabText = Color(0xFF8D8D8D);
-  static const Color textDefault = Color(0xFF212121);
+  static const Color overlay = Color(0xD9000000);
+  static const Color selected = Color(0xFFFFE0B2);
 }
