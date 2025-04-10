c2e622e03c0aa7ceb57002def5d9eb17a84f9574
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-10 18:10:18 +0900

    refactor: RamenCardImage 위젯 분리

--- lib/presentation/home/widget/ramen/ramen_card.dart
+++ lib/presentation/home/widget/ramen/ramen_card.dart
@@ -1,37 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:noodle_timer/domain/entity/ramen_entity.dart';
 import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
+import 'package:noodle_timer/presentation/common/ramen_card_image.dart';
 
-class RamenCard extends StatefulWidget {
+class RamenCard extends StatelessWidget {
   final RamenEntity ramen;
 
   const RamenCard({
     required this.ramen,
-    super.key
+    super.key,
   });
-
-  @override
-  State<RamenCard> createState() => _RamenCardState();
-}
-
-class _RamenCardState extends State<RamenCard> {
-  bool _isLoading = true;
-  bool _hasError = false;
-  late final ImageProvider _imageProvider;
-  ImageStream? _imageStream;
-  ImageStreamListener? _listener;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadImage();
-  }
-
-  @override
-  void dispose() {
-    _removeListeners();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,68 +21,8 @@ class _RamenCardState extends State<RamenCard> {
       ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(8),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [_buildContent(), if (_isLoading) _buildLoadingIndicator()],
-        ),
+        child: RamenCardImage(imageUrl: ramen.imageUrl)
       ),
     );
-  }
-
-  Widget _buildContent() {
-    if (_hasError) {
-      return const Center(
-        child: Icon(Icons.broken_image, color: Colors.white54, size: 40),
-      );
-    }
-
-    return Transform.scale(
-      scale: 0.8,
-      child: Image(image: _imageProvider, fit: BoxFit.cover),
-    );
-  }
-
-  Widget _buildLoadingIndicator() {
-    return const Center(
-      child: CircularProgressIndicator(
-        color: NoodleColors.primary,
-        strokeWidth: 3.0,
-      ),
-    );
-  }
-
-  void _loadImage() {
-    _imageProvider = NetworkImage(widget.ramen.imageUrl);
-    _removeListeners();
-
-    _listener = ImageStreamListener(
-          (ImageInfo image, bool synchronousCall) {
-        if (mounted) {
-          setState(() {
-            _isLoading = false;
-            _hasError = false;
-          });
-        }
-      },
-      onError: (Object exception, StackTrace? stackTrace) {
-        if (mounted) {
-          setState(() {
-            _isLoading = false;
-            _hasError = true;
-          });
-        }
-      },
-    );
-
-    _imageStream = _imageProvider.resolve(const ImageConfiguration());
-    _imageStream?.addListener(_listener!);
-  }
-
-  void _removeListeners() {
-    if (_imageStream != null && _listener != null) {
-      _imageStream?.removeListener(_listener!);
-    }
-    _listener = null;
-    _imageStream = null;
   }
 }
