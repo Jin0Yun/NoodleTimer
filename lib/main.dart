20f5eb4f718d57036456b261a8fe62375f554420
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-13 05:49:52 +0900

    chore: Firebase 연동 초기 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:noodle_timer/firebase_options.dart';
 import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
 import 'package:noodle_timer/presentation/onboarding/screen/noodle_preference_screen.dart';
 import 'package:noodle_timer/presentation/tabbar/screen/tabbar_controller.dart';
@@ -7,6 +9,9 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   final prefs = await SharedPreferences.getInstance();
   final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
 
@@ -14,10 +19,11 @@ void main() async {
     ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          scaffoldBackgroundColor: NoodleColors.neutral100
-        ),
-        home: isFirstLaunch ? const NoodlePreferenceScreen() : const TabBarController(),
+        theme: ThemeData(scaffoldBackgroundColor: NoodleColors.neutral100),
+        home:
+            isFirstLaunch
+                ? const NoodlePreferenceScreen()
+                : const TabBarController(),
       ),
     ),
   );
