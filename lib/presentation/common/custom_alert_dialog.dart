5d7952bc55ec60c1c9136fcdb6cbed4dcc51a920
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-13 00:23:55 +0900

    feat: 다이얼로그 UI 컴포넌트 구현

---
+++ lib/presentation/common/custom_alert_dialog.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:noodle_timer/presentation/common/custom_dialog_button.dart';
+import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
+import 'package:noodle_timer/presentation/common/theme/noodle_text_styles.dart';
+
+class CustomAlertDialog extends StatelessWidget {
+  final String message;
+  final String confirmText;
+  final VoidCallback onConfirm;
+  final bool isSuccess;
+  final bool hasCancel;
+  final String? cancelText;
+  final VoidCallback? onCancel;
+
+  const CustomAlertDialog({
+    super.key,
+    required this.message,
+    required this.confirmText,
+    required this.onConfirm,
+    this.isSuccess = true,
+    this.hasCancel = false,
+    this.cancelText,
+    this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: NoodleColors.neutral100,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              message,
+              textAlign: TextAlign.center,
+              style: NoodleTextStyles.titleXSmBold.copyWith(
+                color: NoodleColors.neutral1000,
+              ),
+            ),
+            const SizedBox(height: 32),
+            if (hasCancel)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomDialogButton(
+                    text: cancelText ?? '취소',
+                    backgroundColor: NoodleColors.neutral200,
+                    textColor: NoodleColors.neutral1000,
+                    onPressed: onCancel!,
+                  ),
+                  const SizedBox(width: 16),
+                  CustomDialogButton(
+                    text: confirmText,
+                    backgroundColor: NoodleColors.primary,
+                    textColor: NoodleColors.neutral100,
+                    onPressed: onConfirm,
+                  ),
+                ],
+              )
+            else
+              CustomDialogButton(
+                text: confirmText,
+                backgroundColor: NoodleColors.primary,
+                textColor: NoodleColors.neutral200,
+                onPressed: onConfirm,
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
