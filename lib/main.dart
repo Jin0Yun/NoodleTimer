4f1fecf00711d224742777cfbb0b60ce1fa833f5
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-13 06:06:28 +0900

    feat: 로그인 화면 UI 구현

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:noodle_timer/firebase_options.dart';
+import 'package:noodle_timer/presentation/auth/screen/login_screen.dart';
 import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
-import 'package:noodle_timer/presentation/onboarding/screen/noodle_preference_screen.dart';
 import 'package:noodle_timer/presentation/tabbar/screen/tabbar_controller.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -22,7 +22,7 @@ void main() async {
         theme: ThemeData(scaffoldBackgroundColor: NoodleColors.neutral100),
         home:
             isFirstLaunch
-                ? const NoodlePreferenceScreen()
+                ? const LoginScreen()
                 : const TabBarController(),
       ),
     ),
