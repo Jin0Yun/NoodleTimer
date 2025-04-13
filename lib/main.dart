308f5852bd0cd69166002d8069dce1ed80eb85be
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-13 23:44:35 +0900

    refactor: 최초 로그인 후 온보딩 UI 전환 구현

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
 import 'package:noodle_timer/presentation/onboarding/screen/noodle_preference_screen.dart';
 import 'package:noodle_timer/presentation/onboarding/screen/onboarding_guide_screen.dart';
 import 'package:noodle_timer/presentation/tabbar/screen/tabbar_controller.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,19 +18,27 @@ void main() async {
 
   final isLoggedIn = FirebaseAuth.instance.currentUser != null;
 
+  final prefs = await SharedPreferences.getInstance();
+  final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+
   runApp(
     ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(scaffoldBackgroundColor: NoodleColors.neutral100),
-        initialRoute: isLoggedIn ? AppRoutes.onboardingPreference : AppRoutes.login,
+        initialRoute:
+            isLoggedIn
+                ? (isFirstLaunch
+                    ? AppRoutes.onboardingPreference
+                    : AppRoutes.home)
+                : AppRoutes.login,
         routes: {
           AppRoutes.home: (_) => const TabBarController(),
           AppRoutes.login: (_) => const LoginScreen(),
           AppRoutes.signup: (_) => const SignUpScreen(),
           AppRoutes.onboardingPreference: (_) => const NoodlePreferenceScreen(),
           AppRoutes.onboardingGuide: (_) => const OnboardingGuideScreen(),
-        }
+        },
       ),
     ),
   );
