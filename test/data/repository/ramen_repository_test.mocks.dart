7dbe176fa46068fdcfe97bafcb895e4873fd83de
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-15 01:54:56 +0900

    refactor: provider 구조 통합 및 의존성 정리

--- test/data/repository/ramen_repository_test.mocks.dart
+++ test/data/repository/ramen_repository_test.mocks.dart
@@ -7,7 +7,7 @@ import 'dart:async' as _i3;
 
 import 'package:mockito/mockito.dart' as _i1;
 import 'package:mockito/src/dummies.dart' as _i4;
-import 'package:noodle_timer/data/data_loader.dart' as _i2;
+import 'package:noodle_timer/data/service/data_loader.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
