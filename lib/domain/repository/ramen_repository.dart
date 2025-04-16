b715929c45dc15518b4efa13569ed15c1d661aa2
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-17 04:26:58 +0900

    feat: 라면 ID 조회 기능 추가

--- lib/domain/repository/ramen_repository.dart
+++ lib/domain/repository/ramen_repository.dart
@@ -4,4 +4,5 @@ import 'package:noodle_timer/domain/entity/ramen_entity.dart';
 abstract class RamenRepository {
   Future<List<RamenBrandEntity>> loadBrands();
   Future<List<RamenEntity>> loadAllRamen();
+  Future<RamenEntity?> findRamenById(int id);
 }
