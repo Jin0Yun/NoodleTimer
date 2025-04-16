fb5c6b41dc22aa68f06a275caa422b1fa5240aac
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-16 12:36:25 +0900

    feat: 라면 선택 브랜드 상태 관리 뷰모델로 전환

--- lib/presentation/home/widget/ramen/ramen_selector_container.dart
+++ lib/presentation/home/widget/ramen/ramen_selector_container.dart
@@ -1,29 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:noodle_timer/core/di/app_providers.dart';
-import 'package:noodle_timer/domain/entity/ramen_brand_entity.dart';
 import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
 import 'package:noodle_timer/presentation/common/theme/noodle_text_styles.dart';
 import 'package:noodle_timer/presentation/common/widget/selectable_tag_list.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:noodle_timer/presentation/home/widget/ramen/ramen_card_list.dart';
 
 class RamenSelectorContainer extends ConsumerWidget {
-  final int selectedCategoryIndex;
-  final ValueChanged<int> onCategoryTap;
-
-  RamenSelectorContainer({
-    required this.selectedCategoryIndex,
-    required this.onCategoryTap,
-    super.key,
-  });
+  const RamenSelectorContainer({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final ramenViewModel = ref.read(ramenViewModelProvider.notifier);
     final ramenState = ref.watch(ramenViewModelProvider);
-    final ramenBrands = ramenState.brands;
-    final selectedBrand = ramenState.brands.isNotEmpty
-        ? ramenState.brands[selectedCategoryIndex]
-        : null;
+    final brands = ramenState.brands;
+    final ramens = ramenState.currentRamenList;
+    final selectedBrandIndex = ramenState.selectedBrandIndex;
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,15 +30,14 @@ class RamenSelectorContainer extends ConsumerWidget {
           ),
         ),
         const SizedBox(height: 8),
-        SelectableTagList<RamenBrandEntity>(
-          selectedIndex: selectedCategoryIndex,
-          onTap: onCategoryTap,
-          items: ramenBrands,
+        SelectableTagList(
+          selectedIndex: selectedBrandIndex,
+          onTap: (index) => ramenViewModel.selectBrand(index),
+          items: brands,
           labelBuilder: (brand) => brand.name,
         ),
         const SizedBox(height: 12),
-        if (selectedBrand != null)
-          RamenCardList(ramens: selectedBrand.ramens),
+        RamenCardList(ramens: ramens),
       ],
     );
   }
