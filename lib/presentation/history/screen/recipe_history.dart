7c828a02fed582a41ffc7602c945c7a678734575
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-17 19:24:24 +0900

    feat: 조리 내역 및 카드 UI 컴포넌트 구현

--- lib/presentation/history/screen/recipe_history.dart
+++ lib/presentation/history/screen/recipe_history.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:noodle_timer/presentation/common/theme/noodle_colors.dart';
+import 'package:noodle_timer/presentation/common/theme/noodle_text_styles.dart';
+import 'package:noodle_timer/presentation/common/widget/custom_search_bar.dart';
+import 'package:noodle_timer/presentation/history/widget/recipe_history_card.dart';
 
 class RecipeHistoryScreen extends StatelessWidget {
-  const RecipeHistoryScreen({super.key});
+  RecipeHistoryScreen({super.key});
+
+  final TextEditingController _searchController = TextEditingController();
+
+  final searchNotifier = _DummySearchNotifier();
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        '조리내역',
-        style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: NoodleColors.neutral100,
+        centerTitle: true,
+        elevation: 0,
+        title: Text(
+          '조리내역',
+          style: NoodleTextStyles.titleSmBold.copyWith(
+            color: NoodleColors.neutral1000,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CustomSearchBar(
+                hintText: '조리했던 라면을 검색해보세요!',
+                controller: _searchController,
+                onClear: () {
+                  _searchController.clear();
+                  searchNotifier.resetSearch();
+                },
+              ),
+              const SizedBox(height: 16),
+              Expanded(
+                child: ListView(
+                  children: [
+                    RecipeHistoryCard(
+                      imageUrl: '',
+                      name: '삼양라면',
+                      cookedTime: '3분 50초 간 조리',
+                      noodleState: '꼬들면',
+                      eggState: '완숙',
+                      onCookAgain: () {},
+                      onDelete: () {},
+                    ),
+                    RecipeHistoryCard(
+                      imageUrl: '',
+                      name: '신라면',
+                      cookedTime: '3분 50초 간 조리',
+                      noodleState: '꼬들면',
+                      eggState: '완숙',
+                      onCookAgain: () {},
+                      onDelete: () {},
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
+  }
+}
+
+class _DummySearchNotifier {
+  void resetSearch() {
+    print("검색 초기화");
   }
 }
