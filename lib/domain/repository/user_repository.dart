3845e7cea51986604cdd4592bcd41baf4641a600
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-04-17 17:33:39 +0900

    feat: Firestore 연동을 위한 UserRepository 구현

---
+++ lib/domain/repository/user_repository.dart
@@ -0,0 +1,12 @@
+import 'package:noodle_timer/domain/entity/cook_history_entity.dart';
+import 'package:noodle_timer/domain/entity/noodle_preference.dart';
+import 'package:noodle_timer/domain/entity/user_entity.dart';
+
+abstract class UserRepository {
+  String? getCurrentUserId();
+  Future<UserEntity?> getUserById(String uid);
+  Stream<UserEntity?> getUserStream(String uid);
+  Future<void> updateNoodlePreference(String uid, NoodlePreference preference);
+  Future<void> saveCookHistory(String uid, CookHistoryEntity history);
+  Future<List<CookHistoryEntity>> getCookHistories(String uid);
+}
