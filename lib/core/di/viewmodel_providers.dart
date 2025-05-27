3db8e9491cf3e785d6ddbde4681503e1f9bcd338
jinyoung <133867430+Jin0Yun@users.noreply.github.com>
2025-05-27 20:03:33 +0900

    refactor: ViewModel Provider 추가 및 불필요한 TimerPhase 로직 삭제

--- lib/core/di/viewmodel_providers.dart
+++ lib/core/di/viewmodel_providers.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:noodle_timer/presentation/state/option_state.dart';
 import 'package:noodle_timer/presentation/state/timer_state.dart';
+import 'package:noodle_timer/presentation/viewmodel/option_view_model.dart';
 import 'package:noodle_timer/presentation/viewmodel/timer_view_model.dart';
 import 'package:noodle_timer/presentation/state/auth_state.dart';
 import 'package:noodle_timer/presentation/viewmodel/auth_view_model.dart';
@@ -7,6 +9,8 @@ import 'package:noodle_timer/presentation/state/history_state.dart';
 import 'package:noodle_timer/presentation/viewmodel/history_view_model.dart';
 import 'package:noodle_timer/presentation/state/ramen_state.dart';
 import 'package:noodle_timer/presentation/viewmodel/ramen_view_model.dart';
+import 'package:noodle_timer/presentation/state/preference_state.dart';
+import 'package:noodle_timer/presentation/viewmodel/preference_view_model.dart';
 import 'core_providers.dart';
 import 'usecase_providers.dart';
 
@@ -30,23 +34,40 @@ final timerViewModelProvider =
     StateNotifierProvider<TimerViewModel, TimerState>((ref) {
       final cookHistoryUseCase = ref.watch(cookHistoryUseCaseProvider);
       final logger = ref.watch(loggerProvider);
-      return TimerViewModel(cookHistoryUseCase, logger, ref);
+      return TimerViewModel(cookHistoryUseCase, logger);
     });
 
 final historyViewModelProvider =
     StateNotifierProvider<HistoryViewModel, HistoryState>((ref) {
-      final userUseCase = ref.watch(userUseCaseProvider);
       final ramenUseCase = ref.watch(ramenUseCaseProvider);
       final cookHistoryUseCase = ref.watch(cookHistoryUseCaseProvider);
-      final firebaseAuth = ref.watch(firebaseAuthProvider);
-      final userId = firebaseAuth.currentUser?.uid ?? '';
       final logger = ref.watch(loggerProvider);
-      return HistoryViewModel(
-        userUseCase,
-        ramenUseCase,
-        cookHistoryUseCase,
-        userId,
-        logger,
-        ref,
-      );
+      return HistoryViewModel(ramenUseCase, cookHistoryUseCase, logger);
+    });
+
+final preferenceViewModelProvider =
+    StateNotifierProvider<PreferenceViewModel, PreferenceState>((ref) {
+      final userUseCase = ref.watch(userUseCaseProvider);
+      final firebaseAuth = ref.watch(firebaseAuthProvider);
+      final userId = firebaseAuth.currentUser?.uid;
+      final logger = ref.watch(loggerProvider);
+
+      if (userId == null) {
+        throw StateError('User not authenticated');
+      }
+      return PreferenceViewModel(userUseCase, userId, logger);
+    });
+
+final optionViewModelProvider =
+    StateNotifierProvider<OptionViewModel, OptionState>((ref) {
+      final userUseCase = ref.watch(userUseCaseProvider);
+      final firebaseAuth = ref.watch(firebaseAuthProvider);
+      final userId = firebaseAuth.currentUser?.uid;
+      final logger = ref.watch(loggerProvider);
+
+      if (userId == null) {
+        throw StateError('User not authenticated');
+      }
+
+      return OptionViewModel(userUseCase, logger);
     });
--- lib/domain/enum/timer_phase.dart
+++ lib/domain/enum/timer_phase.dart
@@ -1,16 +1,1 @@
-import 'package:noodle_timer/presentation/state/timer_state.dart';
-
-enum TimerPhase {
-  initial,
-  cooking,
-  egg,
-  completed,
-}
-
-extension TimerPhaseResolver on TimerState {
-  TimerPhase get phase {
-    if (isInitial) return TimerPhase.initial;
-    if (isCompleted) return TimerPhase.completed;
-    return TimerPhase.cooking;
-  }
-}
+enum TimerPhase { initial, egg, cooking, completed }
